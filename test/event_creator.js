2fba7fb0dc04a8ac4a21c093a0c3ada2e7b6f3c2
Deric Walintukan <deric.walintukan@gmail.com>
2017-08-25 15:39:59 +0700

    Comment out require in constructor for Topic and move init code in EventCreator test

--- test/event_creator.js
+++ test/event_creator.js
@@ -2,19 +2,20 @@ const EventCreator = artifacts.require("./EventCreator.sol");
 
 contract('EventCreator', function(accounts) {
     let eventCreator;
+    let testTopic;
 
     beforeEach(async () => {
         eventCreator = await EventCreator.deployed();
-    })
 
-    it('sets the first account as the contract creator', async function() {
-        const event_creator = await EventCreator.deployed();
         bytes32[] resultNames = new bytes32[](3);
         resultNames[0] = "first";
         resultNames[1] = "second";
         resultNames[3] = "third";
-        const event = await event_creator.createTopic('test', resultNames, 1000000);
 
-        assert.equal(creator, accounts[0], 'main account is the creator')
+        testTopic = await eventCreator.createTopic('test', resultNames, 1000000);
+    })
+
+    it('sets the first account as the contract creator', async function() {
+        assert.equal(testTopic.owner, accounts[0], 'main account is the creator')
     })
 });
