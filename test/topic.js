c2ad943dbf7ede125711712e1e75fb858e0c4779
Deric Walintukan <deric.walintukan@gmail.com>
2017-08-28 15:53:25 +0700

    Add betting end block test

--- test/topic.js
+++ test/topic.js
@@ -19,15 +19,18 @@ contract('Topic', function(accounts) {
 		assert.equal(await testTopic.owner(), accounts[0], "Topic owner does not match.");
     });
 
-    it("sets the topic's name correctly", async function() {
-    	var testTopicName = await testTopic.name();
-		assert.equal(web3.toUtf8(testTopicName), testTopicParams._name, "Topic name does not match.");
+    it("sets the topic name correctly", async function() {
+		assert.equal(web3.toUtf8(await testTopic.name()), testTopicParams._name, "Topic name does not match.");
     });
 
-    it("sets the topic's result names correctly", async function() {
+    it("sets the topic result names correctly", async function() {
     	var resultNames = testTopicParams._resultNames;
 		assert.equal(web3.toUtf8(await testTopic.getResultName(0)), resultNames[0], "Result name 1 does not match.");
 		assert.equal(web3.toUtf8(await testTopic.getResultName(1)), resultNames[1], "Result name 2 does not match.");
 		assert.equal(web3.toUtf8(await testTopic.getResultName(2)), resultNames[2], "Result name 3 does not match.");
+    });
+
+    it("sets the topic betting end block correctly", async function() {
+		assert.equal(await testTopic.bettingEndBlock(), testTopicParams._bettingEndBlock, "Topic betting end block does not match.");
     });
 });
