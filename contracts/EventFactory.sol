cead797f8e40a2b02438937e228f8ce89a8100f3
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-19 10:49:29 +0700

    Remove constructor and pass in msg.sender as topic address

--- contracts/EventFactory.sol
+++ contracts/EventFactory.sol
@@ -2,17 +2,12 @@ pragma solidity ^0.4.11;
 
 import "./Topic.sol";
 
+/// @title Event Factory contract - allows creation of individual prediction events
 contract EventFactory {
-
-    address public owner;
-
-    function EventFactory() {
-        owner = msg.sender;
-    }
     
     function createTopic(bytes32 name, bytes32[] resultNames, uint256 bettingEndBlock)
         returns (Topic tokenAddress)
     {
-        return new Topic(owner, name, resultNames, bettingEndBlock);
+        return new Topic(msg.sender, name, resultNames, bettingEndBlock);
     }
 }
