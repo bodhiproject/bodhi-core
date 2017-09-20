90b4005f5dd18a30c78cd0b9165f9b0184938282
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-20 10:51:49 +0700

    Add mapping for new topics

--- contracts/EventFactory.sol
+++ contracts/EventFactory.sol
@@ -4,10 +4,24 @@ import "./Topic.sol";
 
 /// @title Event Factory contract - allows creation of individual prediction events
 contract EventFactory {
+
+	mapping (bytes32 => Topic) public topics;
+
+	// Events
+	event TopicCreated(address indexed _creator, Topic _topic, bytes32 _name, bytes32[] _resultNames, uint256 _bettingEndBlock);
     
-    function createTopic(bytes32 name, bytes32[] resultNames, uint256 bettingEndBlock)
+    function createTopic(bytes32 _name, bytes32[] _resultNames, uint256 _bettingEndBlock)
+    	public
         returns (Topic tokenAddress)
     {
-        return new Topic(msg.sender, name, resultNames, bettingEndBlock);
+    	bytes32 topicHash = keccak256(_name, _resultNames, _bettingEndBlock);
+    	// Topic should not exist yet
+    	require(address(topics[topicHash]) == 0);
+
+    	Topic topic = new Topic(msg.sender, _name, _resultNames, _bettingEndBlock);
+    	topics[topicHash] = topic;
+
+    	TopicCreated(msg.sender, topic, _name, _resultNames, _bettingEndBlock);
+        return topic;
     }
 }
--- test/event_factory.js
+++ test/event_factory.js
@@ -1,5 +1,50 @@
 const EventFactory = artifacts.require("./EventFactory.sol");
+const BlockHeightManager = require('./helpers/block_height_manager');
+const web3 = global.web3;
+const assert = require('chai').assert;
 
 contract('EventFactory', function(accounts) {
+	const blockHeightManager = new BlockHeightManager(web3);
+	const testTopicParams = {
+		_name: "test",
+		_resultNames: ["first", "second", "third"],
+		_bettingEndBlock: 100
+	};
 
+	let eventFactory;
+	let eventFactoryCreator = accounts[0];
+	let topic;
+
+	beforeEach(blockHeightManager.snapshot);
+  	afterEach(blockHeightManager.revert);
+
+  	beforeEach(async function() {
+  		eventFactory = await EventFactory.deployed({ from: eventFactoryCreator });
+  		
+  		let txn = await eventFactory.createTopic(...Object.values(testTopicParams), { from: accounts[1] });
+  		
+  	});
+
+  	describe('New Topic:', async function() {
+  		it('initializes all the values of the new topic correctly', async function() {
+  			eventFactory = await EventFactory.deployed({ from: eventFactoryCreator });
+  			return await eventFactory.createTopic(...Object.values(testTopicParams), { from: accounts[1] })
+  			.then(function(txn) {
+  				console.log(txn);
+  				topic = Topic.at(txn);
+  				assert.equal(topic.owner.call(), accounts[1], "Topic owner does not match.");
+  			});
+
+  	// 		return EventFactory.deployed({ from: eventFactoryCreator })
+			// .then(function(_eventFactory) {
+			// 	eventFactory = _eventFactory;
+			// 	return eventFactory.createTopic(...Object.values(testTopicParams), { from: accounts[1] })
+			// 	.then(function(txn) {
+			// 		console.log("createTopic transaction hash: " + txn);
+			// 		assert.equal(txn, accounts[1]);
+			// 		return web3.eth.getTransactionReceipt(txn);
+			// 	})
+			// });
+  		});
+  	});
 });
