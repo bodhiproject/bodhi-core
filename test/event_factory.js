afadf40c8cacc5e1a219278f30428771182a7cba
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-21 21:21:19 +0700

    Add betting test

--- test/event_factory.js
+++ test/event_factory.js
@@ -52,5 +52,35 @@ contract('EventFactory', function(accounts) {
 				assert.match(e.message, /invalid opcode/);
 			}
 		});
+
+		it('allows betting if the bettingEndBlock has not been reached', async function() {
+			assert.isBelow(web3.eth.blockNumber, testTopicParams._bettingEndBlock, 
+				'Current block is greater than or equal to bettingEndBlock.');
+
+			let initialBalance = web3.eth.getBalance(topic.address).toNumber();
+			let betResultIndex = 0;
+
+			let better1 = accounts[2];
+			let betAmount1 = web3.toBigNumber(web3.toWei(1, 'ether'));
+			let better2 = accounts[3];
+			let betAmount2 = web3.toBigNumber(web3.toWei(2, 'ether'));
+			let totalBetAmount = betAmount1.add(betAmount2);
+
+			await topic.bet(betResultIndex, { from: better1, value: betAmount1 });
+			await topic.bet(betResultIndex, { from: better2, value: betAmount2 });
+
+			let newBalance = web3.eth.getBalance(topic.address).toNumber();
+			let difference = newBalance - initialBalance;
+			assert.equal(difference, totalBetAmount, 'New result balance does not match added bet.');
+
+			let resultBalance = await topic.getResultBalance(betResultIndex);
+			assert.equal(resultBalance.toString(), totalBetAmount.toString(), 'Result balance does not match.');
+
+			var betBalance = await topic.getBetBalance(betResultIndex, { from: better1 });
+			assert.equal(betBalance.toString(), betAmount1.toString(), 'Better1 bet balance does not match.');
+
+			betBalance = await topic.getBetBalance(betResultIndex, { from: better2 });
+			assert.equal(betBalance.toString(), betAmount2.toString(), 'Better2 bet balance does not match.');
+	    });
 	});
 });
