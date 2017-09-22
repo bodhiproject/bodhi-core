d5aa49b6de3ec85fc08975af12bace0fbdc2166e
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-22 18:32:07 +0700

    Add functionality of base oracle

--- contracts/oracle/Oracle.sol
+++ contracts/oracle/Oracle.sol
@@ -6,14 +6,38 @@ contract Oracle {
     bool public finalResultSet;
     uint public finalResultIndex;
 
-    /// @notice Check to see if the Oracle has set the final result
-    /// @return Boolean if final result is set by Oracle
+    // Events
+    OracleOwnerReplaced(address indexed _newOwner);
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    /// @notice Current owner of Oracle can assign a new Oracle.
+    function replaceOwner(address _newOwner) public onlyOwner validAddress(_newOwner) {
+        require(!finalResultSet);
+        owner = _newOwner;
+        OracleOwnerReplaced(_newOwner);
+    }
+
+    /// @dev Abstract function that Oracles should implement. Should check if _finalResultIndex is valid.
+    function setFinalResult(uint _finalResultIndex) public onlyOwner;
+
+    /// @notice Check to see if the Oracle has set the final result.
+    /// @return Boolean if final result is set by Oracle.
     function isFinalResultSet() public constant returns (bool) {
         return finalResultSet;
     }
 
-    /// @notice Gets the final result index set by Oracle
-    /// @return The index of the final result set by Oracle
+    /// @notice Gets the final result index set by Oracle.
+    /// @return The index of the final result set by Oracle.
     function getFinalResultIndex() public constant returns (uint) {
         return finalResultIndex;
     }
