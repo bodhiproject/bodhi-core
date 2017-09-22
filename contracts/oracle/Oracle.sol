485b5a92575f97314eac1c3584e904a6d7a539e1
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-22 18:20:43 +0700

    Add functionality of base oracle class

--- contracts/oracle/Oracle.sol
+++ contracts/oracle/Oracle.sol
@@ -1,7 +1,20 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.15;
 
-contract oracle/Oracle {
-  function oracle/Oracle() {
-    // constructor
-  }
+/// @title Base Oracle contract
+contract Oracle {
+    address public owner;
+    bool public finalResultSet;
+    uint public finalResultIndex;
+
+    /// @notice Check to see if the Oracle has set the final result
+    /// @return Boolean if final result is set by Oracle
+    function isFinalResultSet() public constant returns (bool) {
+        return finalResultSet;
+    }
+
+    /// @notice Gets the final result index set by Oracle
+    /// @return The index of the final result set by Oracle
+    function getFinalResultIndex() public constant returns (uint) {
+        return finalResultIndex;
+    }
 }
