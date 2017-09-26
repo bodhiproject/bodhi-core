b0389ee34f045f7b32eed094f85aef1abe0d5971
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-26 17:32:30 +0700

    Add calculations for arbitration option end blocl

--- contracts/oracles/Oracle.sol
+++ contracts/oracles/Oracle.sol
@@ -12,25 +12,18 @@ contract Oracle {
         uint8 resultIndex;
     }
 
-    // Number of decimals of token used to create Oracle
-    uint256 public constant nativeDecimals = 18;
-
-    // Number of decimals for BOT
-    uint256 public constant botDecimals = 8;
-
-    // Minimum amount needed to create Oracle
-    uint256 public constant minBaseReward = 1 * (10**nativeDecimals); 
-
-    // Maximum amount of BOT staking contributions allowed
-    uint256 public constant maxStakeContribution = 101 * (10**botDecimals);
+    uint256 public constant nativeDecimals = 18; // Number of decimals of token used to create Oracle
+    uint256 public constant botDecimals = 8; // Number of decimals for BOT
+    uint256 public constant minBaseReward = 1 * (10**nativeDecimals); // Minimum amount needed to create Oracle
+    uint256 public constant maxStakeContribution = 101 * (10**botDecimals); // Maximum amount of BOT staking contributions allowed
 
     bytes32 public eventName;
     bytes32[] public eventResultNames;
     uint256 public eventBettingEndBlock;
 
-    // Block number when Oracle participants can no longer set decision
-    uint256 public decisionEndBlock;
-
+    uint256 public decisionEndBlock; // Block number when Oracle participants can no longer set a result
+    uint256 public arbitrationOptionEndBlock; // Block number when Oracle participants can no longer start arbitration
+    
     uint256 public totalStakeContributed;
     uint16[] public votedResultCount;
 
@@ -38,7 +31,7 @@ contract Oracle {
 
     // Events
     event OracleCreated(bytes32 _eventName, bytes32[] _eventResultNames, uint256 _eventBettingEndBlock, 
-        uint256 _decisionEndBlock, uint256 _baseRewardAmount);
+        uint256 _decisionEndBlock, uint256 _arbitrationOptionEndBlock, uint256 _baseRewardAmount);
     event ParticipantVoted(address _participant, uint256 _stakeContributed, uint8 _resultIndex);
 
     /// @notice Creates new Oracle contract. Requires payment of the minBaseReward. 
@@ -50,7 +43,9 @@ contract Oracle {
         bytes32 _eventName, 
         bytes32[] _eventResultNames, 
         uint256 _eventBettingEndBlock,
-        uint256 _decisionEndBlock) 
+        uint256 _decisionEndBlock,
+        uint8 _averageBlockTime,
+        uint256 _arbitrationOptionMinutes) 
         public
         payable
     {
@@ -58,6 +53,8 @@ contract Oracle {
         require(_eventName.length > 0);
         require(_eventResultNames.length > 1);
         require(_decisionEndBlock > _eventBettingEndBlock);
+        require(_averageBlockTime > 0);
+        require(_arbitrationOptionMinutes > 0);
 
         eventName = _eventName;
 
@@ -68,7 +65,12 @@ contract Oracle {
         eventBettingEndBlock = _eventBettingEndBlock;
         decisionEndBlock = _decisionEndBlock;
 
-        OracleCreated(_eventName, _eventResultNames, _eventBettingEndBlock, _decisionEndBlock, msg.value);
+        uint256 arbitrationBlocks = getArbitrationOptionBlocks(_averageBlockTime, _arbitrationOptionMinutes);
+        arbitrationOptionEndBlock = decisionEndBlock.add(arbitrationBlocks);
+        assert(arbitrationOptionEndBlock > decisionEndBlock);
+
+        OracleCreated(_eventName, _eventResultNames, _eventBettingEndBlock, _decisionEndBlock, 
+            arbitrationOptionEndBlock, msg.value);
     }
 
     /// @notice Vote an Event result which requires BOT payment.
@@ -89,6 +91,19 @@ contract Oracle {
         votedResultCount[_eventResultIndex] += 1;
 
         ParticipantVoted(msg.sender, msg.value, _eventResultIndex);
+    }
+
+    /// @notice Gets the number of blocks allowed for arbitration.
+    /// @param _averageBlockTime The current average mining block time.
+    /// @param _arbitrationOptionMinutes The number of minutes allowed for initiating arbitration.
+    function getArbitrationOptionBlocks(
+        uint8 _averageBlockTime, 
+        uint256 _arbitrationOptionMinutes) 
+        public 
+        constant 
+        returns(uint256) 
+    {
+        return _arbitrationOptionMinutes.div(uint256(_averageBlockTime));
     }
 
     /// @notice Gets the stake contributed by the Oracle participant.
