ff648b327fbf1f9d46ade31c7384a4243d178113
Deric Walintukan <deric.walintukan@gmail.com>
2017-09-27 15:42:56 +0700

    Add oracle test and rename topic test

---
+++ test/oracle.js
@@ -0,0 +1,7 @@
+const web3 = global.web3;
+const Oracle = artifacts.require("./Oracle.sol");
+const assert = require('chai').assert;
+const BlockHeightManager = require('./helpers/block_height_manager');
+
+contract('Oracle', function(accounts) {
+});
