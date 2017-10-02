89643292ffc82796cb63c2bdac7af801a32cbb4d
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-02 23:28:24 +0700

    Finish tests for setting bodhi token

--- test/adddress_manager.js
+++ test/adddress_manager.js
@@ -4,6 +4,7 @@ const assert = require('chai').assert;
 
 contract("AdddressManager", function(accounts) {
     const owner = accounts[0];
+    const tokenAddress = "0x1111111111111111111111111111111111111111";
 
     let instance;
 
@@ -11,9 +12,36 @@ contract("AdddressManager", function(accounts) {
         instance = await AdddressManager.new({ from: owner });
     });
 
-    it("should deploy correctly", async function(done) {
-        var adddress_manager = AdddressManager.deployed();
-        assert.isTrue(true);
-        done();
+    describe("BodhiTokenAddress", async function() {
+        it("should return the correct address if set", async function() {
+            assert.equal(await instance.getBodhiTokenAddress(), 0, "Token address should be unset");
+
+            await instance.setBodhiTokenAddress(tokenAddress, { from: owner });
+            assert.equal(await instance.getBodhiTokenAddress(), tokenAddress, "Token address does not match");
+        });
+
+        it("throws if setting the address from not the owner", async function() {
+            assert.equal(await instance.getBodhiTokenAddress(), 0, "Token address should be unset");
+
+            try {
+                await instance.setBodhiTokenAddress(tokenAddress, { from: accounts[1] });
+                assert.fail();
+            } catch(e) {
+                assert.match(e.message, /invalid opcode/);
+            }
+
+            assert.equal(await instance.getBodhiTokenAddress(), 0, "Token address should still be unset");
+        });
+
+        it("throws if trying to set an invalid address", async function() {
+            assert.equal(await instance.getBodhiTokenAddress(), 0, "Token address should be unset");
+
+            try {
+                await instance.setBodhiTokenAddress(0, { from: owner });
+                assert.fail();
+            } catch(e) {
+                assert.match(e.message, /invalid opcode/);
+            }
+        });
     });
 });
