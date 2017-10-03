4592844ebf0a16df8ee34a1e8a341072874b82b9
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-03 19:02:04 +0700

    Remove unused code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const SafeMath = artifacts.require("./libs/SafeMath.sol");
+const AddressManager = artifacts.require("./addressmanager/AddressManager.sol");
+const EventFactory = artifacts.require("./events/EventFactory.sol");
+const TopicEvent = artifacts.require("./events/TopicEvent.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+    deployer.deploy(AddressManager);
+    deployer.deploy(SafeMath);
+    deployer.link(SafeMath, [TopicEvent, EventFactory]);
+    deployer.deploy(EventFactory);
 };
