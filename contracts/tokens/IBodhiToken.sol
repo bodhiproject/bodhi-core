6f899838f1e65e2ee2598dad4fe960b31904c941
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-04 21:49:33 +0700

    Rename to IBodhiToken

--- contracts/tokens/IBodhiToken.sol
+++ contracts/tokens/IBodhiToken.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.4.15;
 
 /// @title BodhiToken interface contract
 /// @dev This contract defines the allowed methods to be called externally.
-contract BodhiToken {
+contract IBodhiToken {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool);
 }
