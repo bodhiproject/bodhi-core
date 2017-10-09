22d93e4e04578be48111acd8f50c41ff989fa567
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-09 18:00:34 +0700

    Add first oracle factory test

--- test/oracle_factory.js
+++ test/oracle_factory.js
@@ -6,9 +6,42 @@ const assert = require('chai').assert;
 const web3 = global.web3;
 
 contract('OracleFactory', function(accounts) {
-  it("should assert true", function(done) {
-    var oracle_factory = OracleFactory.deployed();
-    assert.isTrue(true);
-    done();
-  });
+    const blockHeightManager = new BlockHeightManager(web3);
+    const testParams = {
+        _eventName: "Test Oracle",
+        _eventResultNames: ["first", "second", "third"],
+        _eventBettingEndBlock: 100,
+        _decisionEndBlock: 120,
+        _arbitrationOptionEndBlock: 140
+    };
+
+    let oracleFactory;
+    let oracleFactoryCreator = accounts[0];
+    let oracle;
+    let oracleCreator = accounts[1];
+
+    beforeEach(blockHeightManager.snapshot);
+    afterEach(blockHeightManager.revert);
+
+    beforeEach(async function() {
+        oracleFactory = await OracleFactory.deployed({ from: oracleFactoryCreator });
+        let transaction = await oracleFactory.createOracle(...Object.values(testParams), { from: oracleCreator });
+        oracle = await Oracle.at(Utils.getParamFromTransaction(transaction, "_oracle"));
+    });
+
+    describe("createOracle", async function() {
+        it('initializes all the values of the new Oracle correctly', async function() {
+            assert.equal(await oracle.owner.call(), oracleCreator, "owner does not match");
+            assert.equal(web3.toUtf8(await oracle.eventName.call()), testParams._eventBettingEndBlock, 
+                "eventName does not match");
+            // assert.equal(web3.toUtf8(await topic.getResultName(0)), testTopicParams._resultNames[0], 
+            //     'Result name 1 does not match.');
+            // assert.equal(web3.toUtf8(await topic.getResultName(1)), testTopicParams._resultNames[1],
+            //     'Result name 2 does not match.');
+            // assert.equal(web3.toUtf8(await topic.getResultName(2)), testTopicParams._resultNames[2],
+            //     'Result name 3 does not match.');
+            assert.equal(await oracle.eventBettingEndBlock.call(), testParams._eventBettingEndBlock,
+                "eventBettingEndBlock does not match");
+        });
+    });
 });
