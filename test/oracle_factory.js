10e36883adcf333bb240d89bd3fe5941b2bf0c41
Deric Walintukan <deric.walintukan@gmail.com>
2017-10-09 20:45:20 +0700

    Add test for voting

--- test/oracle_factory.js
+++ test/oracle_factory.js
@@ -2,8 +2,10 @@ const OracleFactory = artifacts.require('./oracles/OracleFactory.sol');
 const Oracle = artifacts.require('./oracles/Oracle.sol');
 const BlockHeightManager = require('./helpers/block_height_manager');
 const Utils = require('./helpers/utils');
+const bluebird = require('bluebird');
 const assert = require('chai').assert;
 const web3 = global.web3;
+const ethAsync = bluebird.promisifyAll(web3.eth);
 
 contract('OracleFactory', function(accounts) {
     // These should match the decimals in the Oracle contract.
@@ -11,6 +13,12 @@ contract('OracleFactory', function(accounts) {
     const botDecimals = 8;
 
     const blockHeightManager = new BlockHeightManager(web3);
+    const oracleFactoryCreator = accounts[0];
+    const oracleCreator = accounts[1];
+    const participant1 = accounts[2];
+    const participant2 = accounts[3];
+    const participant3 = accounts[4];
+
     const testParams = {
         _eventName: 'Test Oracle',
         _eventResultNames: ['first', 'second', 'third'],
@@ -18,9 +26,8 @@ contract('OracleFactory', function(accounts) {
         _decisionEndBlock: 120,
         _arbitrationOptionEndBlock: 140
     };
-    const oracleFactoryCreator = accounts[0];
-    const oracleCreator = accounts[1];
     const baseReward = Utils.getBigNumberWithDecimals(10, nativeDecimals);
+    const validVotingBlock = testParams._eventBettingEndBlock;
 
     let oracleFactory;
     let oracle;
@@ -54,6 +61,11 @@ contract('OracleFactory', function(accounts) {
                 'arbitrationOptionEndBlock does not match');
         });
 
+        it('sets the baseReward', async function() {
+            let balance = await web3.eth.getBalance(oracle.address);
+            assert.equal(balance.toString(), baseReward.toString(), 'baseReward does not match');
+        });
+
         it('does not allow recreating the same Oracle twice', async function() {
             let oracleExists = await oracleFactory.doesOracleExist(...Object.values(testParams));
             assert.isTrue(oracleExists, 'Oracle should already exist');
@@ -64,6 +76,32 @@ contract('OracleFactory', function(accounts) {
             } catch(e) {
                 assert.match(e.message, /invalid opcode/);
             }
+        });
+
+        it("allows voting if the decisionEndBlock has not been reached", async function() {
+            await blockHeightManager.mineTo(validVotingBlock);
+            let blockNumber = await ethAsync.getBlockNumberAsync();
+            assert(blockNumber >= (await oracle.eventBettingEndBlock.call()).toNumber(), 
+                "Block should be at or after eventBettingEndBlock");
+            assert.isBelow(blockNumber, (await oracle.decisionEndBlock.call()).toNumber(), 
+                "Block should be below decisionEndBlock");
+
+            assert.equal(await oracle.getStakeContributed({ from: participant1 }), 0, 
+                "participant1 should have 0 stakeContributed");
+            assert.isFalse(await oracle.didSetResult({ from: participant1 }), 
+                "participant1 should not have set result");
+            assert.equal(await oracle.totalStakeContributed.call(), 0, "totalStakeContributed should be 0");
+
+            let votedResultIndex = 2;
+            let stakeContributed = Utils.getBigNumberWithDecimals(3, botDecimals);
+            await oracle.voteResult(votedResultIndex, { from: participant1, value: stakeContributed });
+
+            let actualStakeContributed = await oracle.getStakeContributed({ from: participant1 });
+            assert.equal(actualStakeContributed.toString(), stakeContributed.toString(), 
+                "participant1 stakeContributed does not match");
+            assert.isTrue(await oracle.didSetResult({ from: participant1 }), "participant1 should have set result");
+            assert.equal(await oracle.getVotedResultIndex({ from: participant1 }), votedResultIndex,
+                "participant1 voted resultIndex does not match");
         });
     });
 });
