5b32dc094df3f5e9356d683c658a55c4085f8230
Deric Walintukan <deric.walintukan@gmail.com>
2017-11-02 16:57:31 +0700

    Remove idutils from migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,4 @@
 const AddressManager = artifacts.require("./addressmanager/AddressManager.sol");
-const IdUtils = artifacts.require("./libs/IdUtils.sol");
 const EventFactory = artifacts.require("./events/EventFactory.sol");
 const OracleFactory = artifacts.require("./oracles/OracleFactory.sol");
 const SafeMath = artifacts.require("./libs/SafeMath.sol");
@@ -8,8 +7,6 @@ const Oracle = artifacts.require("./oracles/Oracle.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(AddressManager);
-    deployer.deploy(IdUtils);
-    deployer.link(IdUtils, [EventFactory, OracleFactory]);
     deployer.deploy(EventFactory);
     deployer.deploy(OracleFactory);
     deployer.deploy(SafeMath);
