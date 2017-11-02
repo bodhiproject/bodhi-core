7da8840fe90f8411073a868e65e0272998130053
Deric Walintukan <deric.walintukan@gmail.com>
2017-11-02 21:48:16 +0700

    Finish iaddressmanager

--- contracts/storage/IAddressManager.sol
+++ contracts/storage/IAddressManager.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.4.18;
 contract IAddressManager {
     function getBodhiTokenAddress() public constant returns (address);
     function setBodhiTokenAddress(address _tokenAddress) public;
+    function getEventFactoryAddress(uint8 _indexOfAddress) public constant returns (address);
+    function setEventFactoryAddress(uint8 _indexOfAddress, address _newContractAddress) public;
+    function getOracleFactoryAddress(uint8 _indexOfAddress) public constant returns (address);
+    function setOracleFactoryAddress(uint8 _indexOfAddress, address _newContractAddress) public;
 }
