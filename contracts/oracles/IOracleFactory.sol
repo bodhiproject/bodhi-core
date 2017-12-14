b192fe2f328b9431008e4a7853da9994090db62e
Deric Walintukan <deric.walintukan@gmail.com>
2017-12-14 15:16:14 +0700

    Change eventName to name and eventResultNames to resultNames in emitted events

--- contracts/oracles/IOracleFactory.sol
+++ contracts/oracles/IOracleFactory.sol
@@ -1,11 +1,24 @@
 pragma solidity ^0.4.18;
 
 contract IOracleFactory {
-    function createCentralizedOracle(address _oracle, address _eventAddress, bytes32[10] _eventName, 
-        bytes32[10] _eventResultNames, uint8 _numOfResults, uint256 _bettingEndBlock, uint256 _resultSettingEndBlock,
-        uint256 _consensusThreshold) public returns (address);
+    function createCentralizedOracle(
+        address _oracle, 
+        address _eventAddress, 
+        bytes32[10] _eventName, 
+        bytes32[10] _eventResultNames, 
+        uint8 _numOfResults, 
+        uint256 _bettingEndBlock, 
+        uint256 _resultSettingEndBlock,
+        uint256 _consensusThreshold) 
+        public returns (address);
 
-    function createDecentralizedOracle(address _eventAddress, bytes32[10] _eventName, bytes32[10] _eventResultNames, 
-        uint8 _numOfResults, uint8 _lastResultIndex, uint256 _arbitrationEndBlock, uint256 _consensusThreshold) 
+    function createDecentralizedOracle(
+        address _eventAddress, 
+        bytes32[10] _eventName, 
+        bytes32[10] _eventResultNames, 
+        uint8 _numOfResults, 
+        uint8 _lastResultIndex, 
+        uint256 _arbitrationEndBlock, 
+        uint256 _consensusThreshold) 
         public returns (address);
 }
