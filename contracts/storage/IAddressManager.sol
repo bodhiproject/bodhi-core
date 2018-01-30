50dcd57fc87197522a1f6c1dfbdb1c227ce02f65
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-30 14:04:56 +0700

    Change arbitration block length and use uint256

--- contracts/storage/IAddressManager.sol
+++ contracts/storage/IAddressManager.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.18;
 
 contract IAddressManager {
-    uint16 public arbitrationBlockLength;
     uint16 public currentEventFactoryIndex;
     uint16 public currentOracleFactoryIndex;
     address public bodhiTokenAddress;
+    uint256 public arbitrationLength;
     uint256 public startingOracleThreshold;
     uint256 public consensusThresholdIncrement;
 
