b568a89d7dbb4b2c8d1716decfce2f078fd3f458
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-18 20:15:23 +0700

    Change compiled script and recompile

--- compile_all.sh
+++ compile_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'Compiling AddressManager.sol into /compiled'
-solc --optimize --bin --abi --hashes --allow-paths contracts/libs/* -o compiled --overwrite contracts/storage/AddressManager.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/tokens -o compiled --overwrite contracts/storage/AddressManager.sol
 
 echo 'Compiling EventFactory.sol into /compiled'
-solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs/*,contracts/storage/* -o compiled --overwrite contracts/events/EventFactory.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/storage -o compiled --overwrite contracts/events/EventFactory.sol
 
 echo 'Compiling OracleFactory.sol into /compiled'
 solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/storage -o compiled --overwrite contracts/oracles/OracleFactory.sol
