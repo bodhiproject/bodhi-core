7f4f57b3f6a03b2e6b5da06c9d040d24bbee6193
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-21 20:21:07 +0700

    Remove compiled files

--- compile_all.sh
+++ compile_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'Compiling AddressManager.sol into /compiled'
+echo 'Compiling AddressManager.sol into /build'
 solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/tokens -o build --overwrite contracts/storage/AddressManager.sol
 
-echo 'Compiling EventFactory.sol into /compiled'
+echo 'Compiling EventFactory.sol into /build'
 solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/storage -o build --overwrite contracts/events/EventFactory.sol
 
-echo 'Compiling OracleFactory.sol into /compiled'
+echo 'Compiling OracleFactory.sol into /build'
 solc ..=.. --optimize --bin --abi --hashes --allow-paths contracts/libs,contracts/storage -o build --overwrite contracts/oracles/OracleFactory.sol
